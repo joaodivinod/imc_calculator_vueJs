42b254cb79e9cb433cb8e83d20555ddba744417e
João Divino <joaodivinod@gmail.com>
2021-04-07 22:02:49 -0300

    Arquivos Base criados

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Imc></Imc>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+require('@/assets/css/main.css')
+import Imc from './components/imc'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Imc
   }
 }
 </script>
---
+++ src/components/imc.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="content">
+        <header></header>
+        <main></main>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Imc"
+}
+</script>
+
+<style scoped>
+
+</style>
